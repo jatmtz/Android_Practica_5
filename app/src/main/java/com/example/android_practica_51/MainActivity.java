d79557702352336561736bad8770cb1a8758a2c7
jatmtz <jatmtz1215@gmail.com>
2023-10-02 13:45:12 -0600

    Versión 3

--- app/src/main/java/com/example/android_practica_51/MainActivity.java
+++ app/src/main/java/com/example/android_practica_51/MainActivity.java
@@ -2,123 +2,36 @@ package com.example.android_practica_51;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.SearchManager;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
+import android.os.CountDownTimer;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8;
-    private Intent intent;
-
-    public void Navegador(View view){
-        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://es.wikipedia.org/wiki/Bob_Esponja"));
-        startActivity(i);
-    }
-
-    public void Mapa(View view){
-        Intent i = new Intent(Intent.ACTION_VIEW,Uri.parse(("geo.25.543515, -103.339433")));
-        startActivity(i);
-    }
-
-    public void Camara(View view){
-        Intent i = new Intent("android.media.action.IMAGE_CAPTURE");
-        startActivity(i);
-    }
-
-    public void MarcarTelefono(View view){
-        Intent i = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:8713077441"));
-        startActivity(i);
-    }
-
-
+    TextView mTextField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextField = (TextView) findViewById(R.id.mTextField);
 
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn2 = (Button) findViewById(R.id.btn2);
-        btn3 = (Button) findViewById(R.id.btn3);
-        btn4 = (Button) findViewById(R.id.btn4);
-        btn5 = (Button) findViewById(R.id.btn5);
-        btn6 = (Button) findViewById(R.id.btn6);
-        btn7 = (Button) findViewById(R.id.btn7);
-        btn8 = (Button) findViewById(R.id.btn8);
-
-
-
-   btn1.setOnClickListener(new View.OnClickListener() {
-       @Override
-       public void onClick(View view) {
-           intent = new Intent(getApplicationContext(),Segunda.class);
-           startActivity(intent);
-       }
-   });
-
-   btn2.setOnClickListener(new View.OnClickListener() {
-       @Override
-       public void onClick(View view) {
-           intent = new Intent(getApplicationContext(),Tercera.class);
-           startActivity(intent);
-       }
-   });
-
-   btn3.setOnClickListener(new View.OnClickListener() {
-       @Override
-       public void onClick(View view) {
-           intent = new Intent(getApplicationContext(),Cuarta.class);
-           startActivity(intent);
-       }
-   });
-
-
-   btn4.setOnClickListener(new View.OnClickListener() {
-       @Override
-       public void onClick(View view) {
-           intent = new Intent(getApplicationContext(),Quinta.class);
-           startActivity(intent);
-       }
-   });
-
-
-        btn5.setOnClickListener(new View.OnClickListener() {
+        CountDownTimer CDT = new CountDownTimer(5000,1000) {
             @Override
-            public void onClick(View view) {
-                BuscarWeb("Batman");
+            public void onTick(long milliUntilFinish) {
+                mTextField.setText("" + milliUntilFinish /1000);
             }
 
-            public void BuscarWeb(String query){
-                Intent intent1 = new Intent(Intent.ACTION_WEB_SEARCH);
-                intent1.putExtra(SearchManager.QUERY, query);
-                if (intent1.resolveActivity(getPackageManager()) !=null){
-                    startActivity(intent1);
-                }
-            }
-        });
-
-        btn6.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Navegador(v);
-            }
-        });
+            public void onFinish() {
+                Intent i = new Intent(getApplicationContext(), Principal.class);
 
-        btn7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Mapa(v);
             }
-        });
+        };
 
-        btn8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Camara(v);
-            }
-        });
+        CDT.start();
 
     }
+
+
+
 }
