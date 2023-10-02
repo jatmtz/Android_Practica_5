1babc64f42c878dca326720a8ca6109da9445e17
jatmtz <jatmtz1215@gmail.com>
2023-10-02 10:27:38 -0600

    Versión 1

--- app/src/main/java/com/example/android_practica_51/MainActivity.java
+++ app/src/main/java/com/example/android_practica_51/MainActivity.java
@@ -2,13 +2,99 @@ package com.example.android_practica_51;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8;
+    private Intent intent;
+
+    public void Navegador(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://es.wikipedia.org/wiki/Bob_Esponja"));
+        startActivity(i);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+
+
+
+   btn1.setOnClickListener(new View.OnClickListener() {
+       @Override
+       public void onClick(View view) {
+           intent = new Intent(getApplicationContext(),Segunda.class);
+           startActivity(intent);
+       }
+   });
+
+   btn2.setOnClickListener(new View.OnClickListener() {
+       @Override
+       public void onClick(View view) {
+           intent = new Intent(getApplicationContext(),Tercera.class);
+           startActivity(intent);
+       }
+   });
+
+   btn3.setOnClickListener(new View.OnClickListener() {
+       @Override
+       public void onClick(View view) {
+           intent = new Intent(getApplicationContext(),Cuarta.class);
+           startActivity(intent);
+       }
+   });
+
+
+   btn4.setOnClickListener(new View.OnClickListener() {
+       @Override
+       public void onClick(View view) {
+           intent = new Intent(getApplicationContext(),Quinta.class);
+           startActivity(intent);
+       }
+   });
+
+
+
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BuscarWeb("Batman");
+
+
+            }
+
+            public void BuscarWeb(String query){
+                Intent intent1 = new Intent(Intent.ACTION_WEB_SEARCH);
+                intent1.putExtra(SearchManager.QUERY, query);
+                if (intent1.resolveActivity(getPackageManager()) !=null){
+                    startActivity(intent1);
+                }
+            }
+
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navegador(View view);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/android_practica_51/Quinta.java
@@ -0,0 +1,16 @@
+package com.example.android_practica_51;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Quinta extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
